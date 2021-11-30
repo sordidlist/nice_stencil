8196de65aae43c6dd157837a6916510c47b12da2
Gregory Poisson <grpo3666@colorado.edu>
2021-11-30 11:13:47 -0700

    added a flashing text animation because obviously

--- animate.py
+++ animate.py
@@ -12,7 +12,19 @@ def typewrite(line, slowest_delay=0.3):
     print()
 
 
+def flashing(line, slowest_delay=0.3, repetitions=10, end_state='off'):
+    for rep in range(repetitions):
+        print(line, end='')
+        sleep(slowest_delay)
+        print('', end='\r')
+        sleep(slowest_delay)
+        if (end_state == 'on') & (rep == (len(range(repetitions)) - 1)):
+            print(line)
+            break
+
+
 if __name__ == '__main__':
     typewrite("testing out a typewriter effect")
     typewrite("testing it out with a faster speed setting", slowest_delay=0.05)
-    # typewrite(colors.fg_3cbae0("testing it out with colors"), slowest_delay=0.14)
+    flashing(colors.bg_d40558(colors.fg_faa028(" transient example ")), repetitions=2)
+    flashing(colors.bg_3cce68(colors.fg_101918(" persistent example ")), repetitions=2, end_state='on')
